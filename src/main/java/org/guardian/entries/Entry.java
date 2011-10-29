0c37c9a9539a90399375fc497b14d3e12fd3e431
Robin Kupper <robbzkupper@web.de>
2011-10-29 20:05:21 +0200

    Added PlayerInfoEntry

---
+++ src/main/java/org/guardian/entries/Entry.java
@@ -0,0 +1,12 @@
+package org.guardian.entries;
+
+
+/**
+ * Class for parsing managing player's {@PlayerSession}s
+ * 
+ * @author DiddiZ
+ */
+public interface Entry
+{
+	public String getMessage();
+}
---
+++ src/main/java/org/guardian/entries/Rollbackable.java
@@ -0,0 +1,11 @@
+package org.guardian.entries;
+
+import java.util.List;
+import org.bukkit.block.BlockState;
+
+public interface Rollbackable
+{
+	public List<BlockState> getRollbackBlockStates();
+
+	public List<BlockState> getRebuildBlockStates();
+}
