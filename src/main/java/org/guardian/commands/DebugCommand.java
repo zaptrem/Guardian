ec5401461df75ebb671c5570e0a724dc62ef6477
7H3LaughingMan <austin.brkich@gmail.com>
2012-04-09 18:32:27 -0500

    Fix Tabs?

--- src/main/java/org/guardian/commands/DebugCommand.java
+++ src/main/java/org/guardian/commands/DebugCommand.java
@@ -8,39 +8,39 @@ import org.guardian.params.SummarizationMode;
 
 public class DebugCommand extends BaseCommand {
 
-	public DebugCommand() {
-		name = "debug";
+    public DebugCommand() {
+        name = "debug";
         usage = "";
         minArgs = 0;
         maxArgs = 0;
-	}
+    }
 
-	@Override
-	public boolean execute() {
-		QueryParams test = new QueryParams();
-		test.actions.add(ActionType.BLOCK_BREAK);
-		test.sum = SummarizationMode.NONE;
-		test.needSubTableBlock = true;
-		test.needPlayer = true;
-		try {
-			plugin.getDatabaseBridge().getEntries(test);
-		} catch (SQLException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		return true;
-	}
+    @Override
+    public boolean execute() {
+        QueryParams test = new QueryParams();
+        test.actions.add(ActionType.BLOCK_BREAK);
+        test.sum = SummarizationMode.NONE;
+        test.needSubTableBlock = true;
+        test.needPlayer = true;
+        try {
+            plugin.getDatabaseBridge().getEntries(test);
+        } catch (SQLException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+        return true;
+    }
 
-	@Override
-	public boolean permission() {
-		// TODO Auto-generated method stub
-		return true;
-	}
+    @Override
+    public boolean permission() {
+        // TODO Auto-generated method stub
+        return true;
+    }
 
-	@Override
-	public void moreHelp() {
-		// TODO Auto-generated method stub
+    @Override
+    public void moreHelp() {
+        // TODO Auto-generated method stub
 
-	}
+    }
 
 }
