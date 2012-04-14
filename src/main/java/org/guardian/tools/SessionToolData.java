497cc7773fda3f8d8840c0ccd80ae1f85ba138ee
md_5 <md_5@bigpond.com>
2012-04-14 11:58:59 +1000

    Blaze rods aren't used for anything, tools off by default

--- src/main/java/org/guardian/tools/SessionToolData.java
+++ src/main/java/org/guardian/tools/SessionToolData.java
@@ -6,7 +6,7 @@ import org.bukkit.entity.Player;
 
 public class SessionToolData {
 
-    private boolean enabled = true;
+    private boolean enabled = false;
     private ToolMode mode;
     private List<String> params;
 
