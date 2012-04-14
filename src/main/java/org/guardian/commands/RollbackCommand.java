755224833724a25a0683ceffb698120df3c3bbb0
7H3LaughingMan <austin.brkich@gmail.com>
2012-04-14 11:44:06 -0500

    Implemented asTask for RollbackCommand

--- src/main/java/org/guardian/commands/RollbackCommand.java
+++ src/main/java/org/guardian/commands/RollbackCommand.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.command.CommandSender;
+import org.guardian.Guardian;
 import org.guardian.entries.Entry;
 import org.guardian.params.QueryParams;
 import org.guardian.params.QueryParamsFactory;
@@ -20,27 +21,41 @@ public class RollbackCommand extends BaseCommand {
 
     @Override
     public boolean execute() {
-        final QueryParams params = new QueryParamsFactory().create(sender, args);
-        Bukkit.getServer().getScheduler().scheduleAsyncDelayedTask(plugin, new Runnable() {
-
-            public void run() {
-                BukkitUtils.sendMessage(sender, ChatColor.BLUE + "Searching for entries");
-                try {
-                    List<Entry> results = plugin.getLog(params);
-                    BukkitUtils.sendMessage(sender, ChatColor.GREEN + "Found " + results.size() + " entries!");
-                    plugin.rollback(results, sender);
-                } catch (SQLException e) {
-                    // TODO Auto-generated catch block
-                    e.printStackTrace();
-                }
-            }
-        });
+        QueryParams params = new QueryParamsFactory().create(sender, args);
+        session.setLastQuery(params);
+        session.setEntryCache(null);
+        plugin.getServer().getScheduler().scheduleAsyncDelayedTask(plugin, asTask(sender, params, plugin));
         return true;
     }
 
     @Override
     public void moreHelp() {
         // TODO display help
+    }
+    
+    public static Runnable asTask(final CommandSender sender, final QueryParams params, final Guardian plugin) {
+        return new Runnable() {
+
+            @Override
+            public void run() {
+                try {
+                    if (!params.silent) {
+                        BukkitUtils.sendMessage(sender, ChatColor.BLUE + "Searching for entries");
+                    }
+                    List<Entry> results = plugin.getLog(params);
+                    if (results.size() > 0) {
+                        BukkitUtils.sendMessage(sender, ChatColor.GREEN + "Found " + results.size() + " entries!");
+                        plugin.rollback(results, sender);
+                    } else {
+                        BukkitUtils.sendMessage(sender, ChatColor.DARK_AQUA + "No results found.");
+                    }
+                } catch (final Exception ex) {
+                    BukkitUtils.severe("Error occurred during rollback", ex);
+                    sender.sendMessage(ChatColor.RED + "Exception, check error log");
+                }
+            }
+            
+        };
     }
 
     @Override
