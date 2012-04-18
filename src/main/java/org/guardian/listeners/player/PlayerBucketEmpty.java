4989a6f4807f0982aeb9e423034f301372b020ea
slade87 <antonh@gmx.de>
2012-04-18 17:01:51 +0200

    Added Empty Bucket Logging If I see this wrong as treating empty bucket like a BlockPlace Action let me know. I could find a Action Type Water Bucket.

--- src/main/java/org/guardian/listeners/player/PlayerBucketEmpty.java
+++ src/main/java/org/guardian/listeners/player/PlayerBucketEmpty.java
@@ -1,13 +1,19 @@
 package org.guardian.listeners.player;
 
+import org.bukkit.Material;
+import org.bukkit.block.Block;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.player.PlayerBucketEmptyEvent;
+import org.guardian.ActionType;
+import org.guardian.entries.BlockEntry;
 import org.guardian.listeners.LoggingListener;
 
 public class PlayerBucketEmpty extends LoggingListener {
 
     @EventHandler(priority = EventPriority.MONITOR, ignoreCancelled = true)
     public void onPlayerBucketEmpty(final PlayerBucketEmptyEvent event) {
+    	Block block = event.getBlockClicked().getRelative(event.getBlockFace());
+     	consumer.queueEntry(new BlockEntry(ActionType.BLOCK_PLACE, event.getPlayer().getName(), block.getLocation(), System.currentTimeMillis(), 0 , (byte) 0 ,  Material.WATER.getId(), block.getData(), PLUGIN));
     }
 }
