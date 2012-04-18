8564bc1016905b7f938611a9871ca07896fe7f9f
slade87 <antonh@gmx.de>
2012-04-18 17:45:57 +0200

    Added Lava-Flow, Water-Flow Seperation from the regular From-To-Event, It would still need the check for Config though maybe md_5 or laughing man you can add it. It still doesnt rollback all the Flow though, still looking at why.

--- src/main/java/org/guardian/listeners/block/BlockFromTo.java
+++ src/main/java/org/guardian/listeners/block/BlockFromTo.java
@@ -1,6 +1,7 @@
 package org.guardian.listeners.block;
 
 import org.bukkit.Location;
+import org.bukkit.Material;
 import org.bukkit.block.Block;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
@@ -17,7 +18,13 @@ public class BlockFromTo extends LoggingListener {
         final Block to = event.getToBlock();
         final Location loc = from.getLocation();
         if (guardian.getConf().isLogged(loc.getWorld().getName(), ActionType.BLOCK_FROM_TO, ENVIRONMENT)) {
-            consumer.queueEntry(new BlockEntry(ActionType.BLOCK_FROM_TO, ENVIRONMENT, loc, System.currentTimeMillis(), from.getTypeId(), from.getData(), to.getTypeId(), to.getData(), PLUGIN));
+        	if (to.getType().compareTo((Material.WATER)) == 0) {
+        		 consumer.queueEntry(new BlockEntry(ActionType.BLOCK_WATER_FLOW, ENVIRONMENT, loc, System.currentTimeMillis(), from.getTypeId(), from.getData(), to.getTypeId(), to.getData(), PLUGIN));	
+        	} else if (to.getType().compareTo((Material.LAVA)) == 0) {
+        		 consumer.queueEntry(new BlockEntry(ActionType.BLOCK_LAVA_FLOW, ENVIRONMENT, loc, System.currentTimeMillis(), from.getTypeId(), from.getData(), to.getTypeId(), to.getData(), PLUGIN));	
+        	} else {
+        		 consumer.queueEntry(new BlockEntry(ActionType.BLOCK_FROM_TO, ENVIRONMENT, loc, System.currentTimeMillis(), from.getTypeId(), from.getData(), to.getTypeId(), to.getData(), PLUGIN));
+        	}
         }
     }
 }
